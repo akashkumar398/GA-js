33ac968dc9adbbd2f99e1211993301f04e0eeea2
akashkumar398 <akashnaman@gmail.com>
2024-08-14 15:21:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ try {
   const nameToGreet = core.getInput('who-to-greet');
   console.log(`Hello ${nameToGreet}!`);
   const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
+  
+  // Set output using the new method
+  console.log(`time=${time}`) >> process.env.GITHUB_OUTPUT;
+
   const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
 } catch (error) {
